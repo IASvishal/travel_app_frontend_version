4d1dee22c5facb1adbd99104edc6b8298968d6a2
Vishal Mishra <vishalmishraji820@gmail.com>
2024-05-16 14:47:35 +0530

    feat : added navbar, category list and hotel listing page

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import "./Navbar.css";
+export const Navbar = () => {
+    return (
+        <header className="heading-main d-flex  align-center">
+
+            <h1 className="heading-one">
+                <a className="link" href="/">H Harbor</a>
+            </h1>
+            <div className="form-container d-flex align-centre cursor-pointer shadow">
+                <span className="form-option">Any Where</span>
+                <span className="border-right-1px"></span>
+                <span className="form-option">Any Week</span>
+                <span className="border-right-1px"></span>
+                <span className="form-option">Add guests</span>
+                <span className="search material-icons-outlined">search</span>
+            </div>
+            <nav className="d-flex align-centre gap-large">
+                <div className="nav d-flex align-centre cursor-pointer">
+                    <span className="material-icons-outlined profile-option menu">menu</span>
+                    <span className="material-icons-outlined profile-option person">person</span>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
