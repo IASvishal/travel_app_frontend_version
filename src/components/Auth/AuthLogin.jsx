9330472e6b19149087b239eb4d851880800a22b5
Vishal Mishra <vishalmishraji820@gmail.com>
2024-05-29 18:45:16 +0530

    removed test credentilas

--- src/components/Auth/AuthLogin.jsx
+++ src/components/Auth/AuthLogin.jsx
@@ -1,15 +1,13 @@
 import "./Auth.css";
 import { validateNumber, validatePassword } from "../../utils";
-import { useAuth } from "../../context";
 import { loginHandler } from "../../services";
+import { useAuth } from "../../context";
+
 let isNumberValid, isPasswordValid;
 
 export const AuthLogin = () => {
-
     const { authDispatch, number, password } = useAuth();
-
-
-
+    
     const handleNumberChange = (event) => {
         isNumberValid = validateNumber(event.target.value);
         if (isNumberValid) {
@@ -36,71 +34,64 @@ export const AuthLogin = () => {
         }
     };
 
-    const handleFormSubmit = (e) => {
+    const handleFormSubmit = async (e) => {
         e.preventDefault();
-
         if (isNumberValid && isPasswordValid) {
-            const { accessToken, username } = loginHandler(number, password);
+            const { accessToken, username } = await loginHandler(number, password);
             authDispatch({
                 type: "SET_ACCESS_TOKEN",
-                payload: accessToken
+                payload: accessToken,
             });
             authDispatch({
-                type: "SET_USERNAME",
-                payload: username
+                type: "SET_USER_NAME",
+                payload: username,
             });
-        };
-        authDispatch({
-            type: "CLEAR_USER_DATA",
-        });
-        authDispatch({
-            type: "SHOW_AUTH_MODAL",
-        })
-    }
 
-    const handleTestCredentialsClicks = async() => {
-        const { accessToken, username } = await loginHandler("7878787878", "abc@1234");
-
-        authDispatch({
-            type: "SET_ACCESS_TOKEN",
-            payload: accessToken
-        });
-        authDispatch({
-            type: "SET_USERNAME",
-            payload: username
-        });
+        }
         authDispatch({
             type: "CLEAR_USER_DATA",
         });
         authDispatch({
             type: "SHOW_AUTH_MODAL",
         });
+    };
 
-    }
 
     return (
         <div className="auth-container">
-            <form action="" onSubmit={handleFormSubmit}>
+            <form onSubmit={handleFormSubmit}>
                 <div className="d-flex direction-column lb-in-container">
-                    <label className="auth-label">Mobile Number <span className="asterisk">*</span></label>
-                    <input className="auth-input" required type="number" onChange={handleNumberChange}
+                    <label className="auth-label">
+                        Mobile Number <span className="asterisk">*</span>{" "}
+                    </label>
+                    <input
                         defaultValue={number}
-                        placeholder="Enter Mobile Number" maxLength="10" />
+                        type="number"
+                        className="auth-input"
+                        maxLength="10"
+                        placeholder="Enter Mobile Number"
+                        required
+                        onChange={handleNumberChange}
+                    />
                 </div>
                 <div className="d-flex direction-column lb-in-container">
-                    <label className="auth-label">Password<span className="asterisk">*</span></label>
-                    <input className="auth-input" required type="password" onChange={handlePasswordChange}
+                    <label className="auth-label">
+                        Password <span className="asterisk">*</span>{" "}
+                    </label>
+                    <input
                         defaultValue={password}
-                        placeholder="Enter Password" />
+                        className="auth-input"
+                        placeholder="Enter Password"
+                        type="password"
+                        required
+                        onChange={handlePasswordChange}
+                    />
                 </div>
                 <div>
                     <button className="button btn-primary btn-login cursor">Login</button>
                 </div>
             </form>
-            <div className="cta">
-
-                <button className="button btn-outline-primary cursor-pointer" onClick={handleTestCredentialsClicks}>Login With Test Credentials</button>
-            </div>
+            
         </div>
-    )
-}
+    );
+};
