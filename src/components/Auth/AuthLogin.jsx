99935cd34f65c4cf361351a2e05a3d806c66036a
Vishal Mishra <vishalmishraji820@gmail.com>
2024-06-13 12:58:37 +0530

    Some changes updated

--- src/components/Auth/AuthLogin.jsx
+++ src/components/Auth/AuthLogin.jsx
@@ -1,13 +1,15 @@
 import "./Auth.css";
 import { validateNumber, validatePassword } from "../../utils";
-import { loginHandler } from "../../services";
 import { useAuth } from "../../context";
-
+import { loginHandler } from "../../services";
 let isNumberValid, isPasswordValid;
 
 export const AuthLogin = () => {
+
     const { authDispatch, number, password } = useAuth();
-    
+
+
+
     const handleNumberChange = (event) => {
         isNumberValid = validateNumber(event.target.value);
         if (isNumberValid) {
@@ -34,80 +36,47 @@ export const AuthLogin = () => {
         }
     };
 
-    const handleFormSubmit = async (e) => {
+    const handleFormSubmit = (e) => {
         e.preventDefault();
+
         if (isNumberValid && isPasswordValid) {
-            const { accessToken, username } = await loginHandler(number, password);
+            const { accessToken, username } = loginHandler(number, password);
             authDispatch({
                 type: "SET_ACCESS_TOKEN",
-                payload: accessToken,
+                payload: accessToken
             });
-            authDispatch({
-                type: "SET_USER_NAME",
-                payload: username,
+        authDispatch({
+                type: "SET_USERNAME",
+                payload: username
             });
-<<<<<<< HEAD
-
-        }
-=======
         };
->>>>>>> 15dac05342062bccd58e906082c05e9fc1d38d3f
         authDispatch({
             type: "CLEAR_USER_DATA",
         });
         authDispatch({
             type: "SHOW_AUTH_MODAL",
-<<<<<<< HEAD
-        });
-    };
-
-
-=======
         })
     } 
->>>>>>> 15dac05342062bccd58e906082c05e9fc1d38d3f
     return (
         <div className="auth-container">
-            <form onSubmit={handleFormSubmit}>
+            <form action="" onSubmit={handleFormSubmit}>
                 <div className="d-flex direction-column lb-in-container">
-                    <label className="auth-label">
-                        Mobile Number <span className="asterisk">*</span>{" "}
-                    </label>
-                    <input
+                    <label className="auth-label">Mobile Number <span className="asterisk">*</span></label>
+                    <input className="auth-input" required type="number" onChange={handleNumberChange}
                         defaultValue={number}
-                        type="number"
-                        className="auth-input"
-                        maxLength="10"
-                        placeholder="Enter Mobile Number"
-                        required
-                        onChange={handleNumberChange}
-                    />
+                        placeholder="Enter Mobile Number" maxLength="10" />
                 </div>
                 <div className="d-flex direction-column lb-in-container">
-                    <label className="auth-label">
-                        Password <span className="asterisk">*</span>{" "}
-                    </label>
-                    <input
+                    <label className="auth-label">Password<span className="asterisk">*</span></label>
+                    <input className="auth-input" required type="password" onChange={handlePasswordChange}
                         defaultValue={password}
-                        className="auth-input"
-                        placeholder="Enter Password"
-                        type="password"
-                        required
-                        onChange={handlePasswordChange}
-                    />
+                        placeholder="Enter Password" />
                 </div>
                 <div>
                     <button className="button btn-primary btn-login cursor">Login</button>
                 </div>
             </form>
-<<<<<<< HEAD
-            
-        </div>
-    );
-};
-=======
 
         </div>
     )
 }
->>>>>>> 15dac05342062bccd58e906082c05e9fc1d38d3f
